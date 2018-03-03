e6c5ea049947d560875a8a6a08ee93dfae2068d1
downdead <mattbford@live.com>
2018-03-03 14:44:03 -0800

    astar test

---
+++ app/old_main.py
@@ -0,0 +1,61 @@
+import bottle
+import os
+import random
+
+
+
+@bottle.route('/')
+def static():
+    return "the server is running"
+
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+    game_id = data.get('game_id')
+    board_width = data.get('width')
+    board_height = data.get('height')
+
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+    # TODO: Do things with data
+
+    return {
+        'color': '#00FF00',
+        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+        'head_url': head_url
+    }
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    directions = ['up', 'down', 'left', 'right']
+    direction = random.choice(directions)
+    print direction
+    return {
+        'move': direction,
+        'taunt': 'Roz, my tender, oozing blossom, youre looking fabulous today. Is that a new haircut? Tell me its a new haircut. Its got to be a new haircut. New makeup? You had a lift? You had a tuck? You had something? Something has been inserted in in you that makes you look... Listen, I need a favor. Randall was working late last night out on the scare floor. I really need the key to the door he was using.'
+    }
+
+
+# Expose WSGI app (so gunicorn can find it)
+application = bottle.default_app()
+
+if __name__ == '__main__':
+    bottle.run(
+        application,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=os.getenv('PORT', '8080'),
+        debug = True)
