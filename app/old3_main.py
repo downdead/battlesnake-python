870671ff17a110691d7678ffc3abb7d53f18df50
downdead <mattbford@live.com>
2018-03-03 15:52:46 -0800

    newest astar

---
+++ app/old3_main.py
@@ -0,0 +1,124 @@
+from AStar import *
+import bottle
+import copy
+import math
+import os
+
+SNEK_BUFFER = 3
+ID = 'de508402-17c8-4ac7-ab0b-f96cb53fbee8'
+SNAKE = 1
+FOOD = 3
+SAFTEY = 3
+
+def dist(p, q):
+    dx = abs(p[0] - q[0])
+    dy = abs(p[1] - q[1])
+    return dx + dy;
+
+def direction(from_cell, to_cell):
+    dx = to_cell[0] - from_cell[0]
+    dy = to_cell[1] - from_cell[1]
+
+    if dx == 1:
+        return 'right'
+    elif dx == -1:
+        return 'left'
+    elif dy == -1:
+        return 'up'
+    elif dy == 1:
+        return 'down'
+
+def closest(items, start):
+    closest_item = None
+    closest_distance = 10000
+
+    # TODO: use builtin min for speed up
+    for item in items:
+        item_distance = distance(start, item)
+        if item_distance < closest_distance:
+            closest_item = item
+            closest_distance = item_distance
+
+    return closest_item
+
+def init(data):
+    grid = [[0 for col in xrange(data['height'])] for row in xrange(data['width'])]
+    for snek in data['snakes']:
+        if snek['id'] == ID:
+            mysnake = snek
+        for coord in snek['coords']:
+            grid[coord[0]][coord[1]] = SNAKE
+
+
+    for f in data['food']:
+        grid[f[0]][f[1]] = FOOD
+
+    return mysnake, grid
+
+@bottle.route('/static/<path:path>')
+def static(path):
+    return bottle.static_file(path, root='static/')
+
+
+@bottle.get('/')
+def index():
+    head_url = '%s://%s/static/Traitor.gif' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+    return {
+        'color': '#00ff00',
+        'head': head_url
+    }
+
+
+@bottle.post('/start')
+def start():
+    data = bottle.request.json
+    game_id = data.get('game_id')
+    board_width = data.get('width')
+    board_height = data.get('height')
+
+    head_url = '%s://%s/static/head.png' % (
+        bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc
+    )
+
+
+    return {
+        'color': '#00FF00',
+        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+        'head_url': head_url
+    }
+
+
+@bottle.post('/move')
+def move():
+    data = bottle.request.json
+    snek, grid = init(data)
+    food = data['food']
+    my_coords = snek['coords']
+    my_head = snek['coords'][0]
+    goal = closest(food, my_head)
+    path = a_star(my_head, goal, grid, snek_coords)
+    return {
+        'move': direction(path[0], path[1]),
+        'taunt': 'insert mike wazowski quote here'
+    }
+
+
+@bottle.post('/end')
+def end():
+    data = bottle.request.json
+
+    # TODO: Do things with data
+
+    return {
+        'taunt': 'battlesnake-python!'
+    }
+
+
+application = bottle.default_app()
+if __name__ == '__main__':
+    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
